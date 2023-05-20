40cdcfcbcd5d2028e0016bfb190c51587680e9db
Fabian Wagner <fabian@blax.at>
2023-05-20 15:12:34 +0200

    I test

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Blax\Wordpress;
+
+class Plugin
+{
+    public function __construct()
+    {
+        echo 'test';
+    }
+
+    public function run()
+    {
+        echo 'run';
+    }
+}
