c2b5a9346f0cad8469a0fe4f2fe3db2b405a785d
Fabian Wagner <fabian@blax.at>
2023-05-21 08:53:01 +0200

    A PHPunit & tests, I Setupservice

--- src/Services/SetupService.php
+++ src/Services/SetupService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Blax\Wordpress\Service;
+namespace Blax\Wordpress\Services;
 
 class SetupService
 {
@@ -17,11 +17,30 @@ class SetupService
         // Replace the existing namespace with the new namespace
         $modifiedContent = preg_replace(
             '/^namespace\s+([^\s;]+)/m',
-            'namespace ' . $newNamespace . ';',
+            'namespace ' . $newNamespace . '',
             $fileContent
         );
 
         // Write the modified content back to the PHP file
         file_put_contents($filePath, $modifiedContent);
+
+        return true;
+    }
+
+    /*
+     * |--------------------------------------------------------------------------
+     * | Get the namespace of a PHP file
+     * |--------------------------------------------------------------------------
+     */
+    public static function getNamespaceOfFile($filePath)
+    {
+        // Read the contents of the PHP file
+        $fileContent = file_get_contents($filePath);
+
+        // Get the namespace from the PHP file
+        preg_match('/^namespace\s+([^\s;]+)/m', $fileContent, $matches);
+
+        // Return the namespace
+        return $matches[1];
     }
 }
---
+++ tests/SetupServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Blax\Wordpress\Tests;
+
+use Blax\Wordpress\Services\SetupService;
+
+class SetupServiceTest extends \PHPUnit\Framework\TestCase
+{
+    public function test_namespaces()
+    {
+        // get current namespace
+        $originalNamespace = SetupService::getNamespaceOfFile(__FILE__);
+        $artificialNamespace = 'Blax\Artificial';
+
+        // assert current namespace
+        $this->assertEquals('Blax\Wordpress\Tests', $originalNamespace);
+
+        $this->assertTrue(SetupService::changeNamespaceOfFile(__FILE__, $artificialNamespace));
+        $this->assertEquals($artificialNamespace, SetupService::getNamespaceOfFile(__FILE__));
+        $this->assertTrue(SetupService::changeNamespaceOfFile(__FILE__, $originalNamespace));
+    }
+}
