21a81cbcb175c74698d0595b4a1dfafa2d139252
Fabian Wagner <fabian@blax.at>
2023-05-30 13:02:56 +0200

    BF  ask with slash

--- src/Extendables/Command.php
+++ src/Extendables/Command.php
@@ -11,192 +11,196 @@ use Symfony\Component\Console\Question\Question;
 
 abstract class Command extends SymfCommand
 {
-    public static $signature;
-    public static $description;
+	public static $signature;
+	public static $description;
 
-    public InputInterface $input;
-    public OutputInterface $output;
+	public InputInterface $input;
+	public OutputInterface $output;
 
-    protected function configure()
-    {
-        $this->setName(static::$signature)
-            ->setDescription(static::$description ?? '[No description]');
+	protected function configure()
+	{
+		$this->setName(static::$signature)
+			->setDescription(static::$description ?? '[No description]');
 
-        $this->configureArguments();
+		$this->configureArguments();
 
-        // ignore too many arguments
-        $this->ignoreValidationErrors();
-    }
+		// ignore too many arguments
+		$this->ignoreValidationErrors();
+	}
 
-    final protected function configureOptions()
-    {
-        $options = $this->input->getOptions();
+	final protected function configureOptions()
+	{
+		$options = $this->input->getOptions();
 
-        preg_match_all('/\{--(\S+)\}/', static::$signature, $matches);
+		preg_match_all('/\{--(\S+)\}/', static::$signature, $matches);
 
-        foreach ($matches[1] as $match) {
-            if (array_key_exists($match, $options)) {
-                $this->writelnColor("Option <fg=yellow>--{$match}</> already exists", 'red');
-                continue;
-            }
+		foreach ($matches[1] as $match) {
+			if (array_key_exists($match, $options)) {
+				$this->writelnColor("Option <fg=yellow>--{$match}</> already exists", 'red');
+				continue;
+			}
 
-            // if match contains "=", set a default value
-            $default = '';
-            if (strpos($match, '=')) {
-                $parts = explode('=', $match);
-                // if contains more than 2 parts throw 
-                if (count($parts) > 2) {
-                    $this->writelnColor("Option default <fg=yellow>--{$match}</> is invalid", 'red');
-                    continue;
-                }
-                $match   = $parts[0];
-                $default = $parts[1];
-            }
+			// if match contains "=", set a default value
+			$default = '';
+			if (strpos($match, '=')) {
+				$parts = explode('=', $match);
+				// if contains more than 2 parts throw
+				if (count($parts) > 2) {
+					$this->writelnColor("Option default <fg=yellow>--{$match}</> is invalid", 'red');
+					continue;
+				}
+				$match   = $parts[0];
+				$default = $parts[1];
+			}
 
-            $option_description = null;
-            if (strpos($match, ':')) {
-                $parts = explode(':', $match);
-                // if contains more than 2 parts throw 
-                if (count($parts) > 2) {
-                    $this->writelnColor("Option description <fg=yellow>--{$match}</> is invalid", 'red');
-                    continue;
-                }
-            }
+			$option_description = null;
+			if (strpos($match, ':')) {
+				$parts = explode(':', $match);
+				// if contains more than 2 parts throw
+				if (count($parts) > 2) {
+					$this->writelnColor("Option description <fg=yellow>--{$match}</> is invalid", 'red');
+					continue;
+				}
+			}
 
-            $this->addOption($match, null, $option_description, $default);
-        }
-    }
+			$this->addOption($match, null, $option_description, $default);
+		}
+	}
 
-    final protected function configureArguments()
-    {
-        preg_match_all('/\{(?!--)(\S+)\}/', static::$signature, $matches);
-
-
-        foreach ($matches[1] as $match) {
-            // if match contains "=", set a default value
-            $default = null;
-            if (strpos($match, '=')) {
-                $parts = explode('=', $match);
-                // if contains more than 2 parts throw 
-                if (count($parts) > 2) {
-                    $this->writelnColor("Argument default <fg=yellow>{$match}</> is invalid", 'red');
-                    continue;
-                }
-                $match   = $parts[0];
-                $default = $parts[1];
-            }
-
-            $description = '';
-            if (strpos($match, ':')) {
-                $parts = explode(':', $match);
-                // if contains more than 2 parts throw 
-                if (count($parts) > 2) {
-                    $this->writelnColor("Argument description <fg=yellow>{$match}</> is invalid", 'red');
-                    continue;
-                }
-            }
-
-            $is_optional = (substr($match, -1) === '?')
-                ? InputArgument::OPTIONAL
-                : InputArgument::REQUIRED;
-
-            $match = (substr($match, -1) === '?')
-                ? substr($match, 0, -1)
-                : $match;
-
-            $this->addArgument($match, $is_optional, $description, $default);
-        }
-    }
+	final protected function configureArguments()
+	{
+		preg_match_all('/\{(?!--)(\S+)\}/', static::$signature, $matches);
 
 
-    final protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->input  = $input;
-        $this->output = $output;
+		foreach ($matches[1] as $match) {
+			// if match contains "=", set a default value
+			$default = null;
+			if (strpos($match, '=')) {
+				$parts = explode('=', $match);
+				// if contains more than 2 parts throw
+				if (count($parts) > 2) {
+					$this->writelnColor("Argument default <fg=yellow>{$match}</> is invalid", 'red');
+					continue;
+				}
+				$match   = $parts[0];
+				$default = $parts[1];
+			}
 
-        $this->configureOptions();
+			$description = '';
+			if (strpos($match, ':')) {
+				$parts = explode(':', $match);
+				// if contains more than 2 parts throw
+				if (count($parts) > 2) {
+					$this->writelnColor("Argument description <fg=yellow>{$match}</> is invalid", 'red');
+					continue;
+				}
+			}
 
-        return $this->handle();
-    }
+			$is_optional = (substr($match, -1) === '?')
+				? InputArgument::OPTIONAL
+				: InputArgument::REQUIRED;
 
-    abstract function handle();
+			$match = (substr($match, -1) === '?')
+				? substr($match, 0, -1)
+				: $match;
 
-    public function write()
-    {
-        $this->output->write(...func_get_args());
-    }
+			$this->addArgument($match, $is_optional, $description, $default);
+		}
+	}
 
-    public function writeln()
-    {
-        $this->output->writeln(...func_get_args());
-    }
 
-    public function writelnColor($messages, $color = 'white', $options = 0)
-    {
-        $this->output->writeln("<fg={$color}>{$messages}</>", $options);
-    }
+	final protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$this->input  = $input;
+		$this->output = $output;
 
-    public function writeColor($messages, $color = 'white', $options = 0)
-    {
-        $this->output->write("<fg={$color}>{$messages}</>", $options);
-    }
+		$this->configureOptions();
 
-    public function ask(String|Question $question, $default = null)
-    {
-        if ($question instanceof Question) {
-            return $this->getHelper('question')->ask($this->input, $this->output, $question);
-        }
+		return $this->handle();
+	}
 
-        $visible_default = $default ? ' [<fg=yellow>' . $default . '</>]' : '';
+	abstract function handle();
 
-        $question = new Question('<fg=green>' . $question . '</>' . $visible_default . PHP_EOL . '> ', $default);
+	public function write()
+	{
+		$this->output->write(...func_get_args());
+	}
 
-        $this->output->writeln('');
-        $answer = $this->getHelper('question')->ask($this->input, $this->output, $question);
-        $this->output->writeln('');
+	public function writeln()
+	{
+		$this->output->writeln(...func_get_args());
+	}
 
-        return $answer;
-    }
+	public function writelnColor($messages, $color = 'white', $options = 0)
+	{
+		$this->output->writeln("<fg={$color}>{$messages}</>", $options);
+	}
 
-    public function confirm(String|ConfirmationQuestion $question, $default = false)
-    {
-        if ($question instanceof ConfirmationQuestion) {
-            return $this->getHelper('question')->ask($this->input, $this->output, $question);
-        }
+	public function writeColor($messages, $color = 'white', $options = 0)
+	{
+		$this->output->write("<fg={$color}>{$messages}</>", $options);
+	}
 
-        $visible_default = $default ? ' [<fg=yellow>yes</>]' : ' [<fg=yellow>no</>]';
+	public function ask(String|Question $question, $default = null)
+	{
+		if ($question instanceof Question) {
+			return $this->getHelper('question')->ask($this->input, $this->output, $question);
+		}
 
-        $question = new ConfirmationQuestion('<fg=green>' . $question . '</> (yes/no)' . $visible_default . PHP_EOL . '> ', $default);
+		if ($default && substr($default, -1) === '\\') {
+			$default = " " . $default . " ";
+		}
 
-        $this->output->writeln('');
-        $answer = $this->getHelper('question')->ask($this->input, $this->output, $question);
-        $this->output->writeln('');
+		$visible_default = $default ? ' [<fg=yellow>' . $default . '</>]' : '';
 
-        return $answer;
-    }
+		$question = new Question('<fg=green>' . $question . '</>' . $visible_default . PHP_EOL . '> ', $default);
 
-    public function getOption($name)
-    {
-        return $this->input->getOption($name);
-    }
+		$this->output->writeln('');
+		$answer = $this->getHelper('question')->ask($this->input, $this->output, $question);
+		$this->output->writeln('');
 
-    public function hasOption($name)
-    {
-        return $this->input->hasOption($name);
-    }
+		return $answer;
+	}
 
-    public function getArguments()
-    {
-        return $this->input->getArguments();
-    }
+	public function confirm(String|ConfirmationQuestion $question, $default = false)
+	{
+		if ($question instanceof ConfirmationQuestion) {
+			return $this->getHelper('question')->ask($this->input, $this->output, $question);
+		}
 
-    public function getArgument($name)
-    {
-        return $this->input->getArgument($name);
-    }
+		$visible_default = $default ? ' [<fg=yellow>yes</>]' : ' [<fg=yellow>no</>]';
 
-    public function hasArgument($name)
-    {
-        return $this->input->hasArgument($name);
-    }
+		$question = new ConfirmationQuestion('<fg=green>' . $question . '</> (yes/no)' . $visible_default . PHP_EOL . '> ', $default);
+
+		$this->output->writeln('');
+		$answer = $this->getHelper('question')->ask($this->input, $this->output, $question);
+		$this->output->writeln('');
+
+		return $answer;
+	}
+
+	public function getOption($name)
+	{
+		return $this->input->getOption($name);
+	}
+
+	public function hasOption($name)
+	{
+		return $this->input->hasOption($name);
+	}
+
+	public function getArguments()
+	{
+		return $this->input->getArguments();
+	}
+
+	public function getArgument($name)
+	{
+		return $this->input->getArgument($name);
+	}
+
+	public function hasArgument($name)
+	{
+		return $this->input->hasArgument($name);
+	}
 }
